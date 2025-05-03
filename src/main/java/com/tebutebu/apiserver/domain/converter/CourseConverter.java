4302eb7c7263eaca1161fe0af74ca782c3fc6a49
DEOKJAE KWON <juintin@kakao.com>
2025-05-03 11:57:21 +0900

    feat: Enum 컨버터 공통화
    - BaseEnumConverter 추상 클래스 추가
    - MemberRoleConverter, CourseConverter 클래스가 BaseEnumConverter 상속하도록 수정
    - 중복 로직 제거 및 코드 간결화

--- src/main/java/com/tebutebu/apiserver/domain/converter/CourseConverter.java
+++ src/main/java/com/tebutebu/apiserver/domain/converter/CourseConverter.java
@@ -1,30 +1,11 @@
 package com.tebutebu.apiserver.domain.converter;
 
 import com.tebutebu.apiserver.domain.Course;
-import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
 
 @Converter(autoApply = true)
-public class CourseConverter implements AttributeConverter<Course, String> {
-
-    @Override
-    public String convertToDatabaseColumn(Course course) {
-        if (course == null) {
-            return null;
-        }
-        return course.name();
-    }
-
-    @Override
-    public Course convertToEntityAttribute(String dbData) {
-        if (dbData == null) {
-            return null;
-        }
-        try {
-            return Course.valueOf(dbData);
-        } catch (IllegalArgumentException e) {
-            return null;
-        }
+public class CourseConverter extends BaseEnumConverter<Course> {
+    public CourseConverter() {
+        super(Course.class);
     }
 }
-
--- src/main/java/com/tebutebu/apiserver/domain/converter/MemberRoleConverter.java
+++ src/main/java/com/tebutebu/apiserver/domain/converter/MemberRoleConverter.java
@@ -1,30 +1,11 @@
 package com.tebutebu.apiserver.domain.converter;
 
 import com.tebutebu.apiserver.domain.MemberRole;
-import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
 
 @Converter(autoApply = true)
-public class MemberRoleConverter implements AttributeConverter<MemberRole, String> {
-
-    @Override
-    public String convertToDatabaseColumn(MemberRole role) {
-        if (role == null) {
-            return null;
-        }
-        return role.name();
+public class MemberRoleConverter extends BaseEnumConverter<MemberRole> {
+    public MemberRoleConverter() {
+        super(MemberRole.class);
     }
-
-    @Override
-    public MemberRole convertToEntityAttribute(String roleName) {
-        if (roleName == null) {
-            return null;
-        }
-        try {
-            return MemberRole.valueOf(roleName);
-        } catch (IllegalArgumentException e) {
-            return null;
-        }
-    }
-
 }
