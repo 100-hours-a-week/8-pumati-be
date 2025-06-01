c6baaca4dab1b2b7f8b0631202e09d7f403d1497
DEOKJAE KWON <juintin@kakao.com>
2025-06-01 22:32:52 +0900

    refactor: PreSignedUrlRequestDTO에 Lombok 애노테이션 추가
    @Builder
    @NoArgsConstructor
    @AllArgsConstructor

--- src/main/java/com/tebutebu/apiserver/dto/s3/request/MultiplePreSignedUrlsRequestDTO.java
+++ src/main/java/com/tebutebu/apiserver/dto/s3/request/MultiplePreSignedUrlsRequestDTO.java
@@ -1,14 +1,21 @@
 package com.tebutebu.apiserver.dto.s3.request;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 import java.util.List;
 
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MultiplePreSignedUrlsRequestDTO {
 
     @NotEmpty(message = "파일 리스트는 필수 입력 값입니다.")
