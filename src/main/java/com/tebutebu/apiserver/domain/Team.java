31da09dc09f1fe2529a7bcdc3839d959db9aeba5
DEOKJAE KWON <juintin@kakao.com>
2025-06-10 17:46:10 +0900

    feat: AI 뱃지 수정 진행 여부 관리 기능 추가 (#143)

--- src/main/java/com/tebutebu/apiserver/domain/Team.java
+++ src/main/java/com/tebutebu/apiserver/domain/Team.java
@@ -45,7 +45,7 @@ public class Team extends TimeStampedEntity {
     private int number;
 
     @OneToMany(mappedBy = "team", cascade = CascadeType.ALL, orphanRemoval = true)
-    List<Member> members;
+    private List<Member> members;
 
     @OneToMany(mappedBy = "team", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<MemberTeamBadge> memberBadges;
@@ -60,6 +60,10 @@ public class Team extends TimeStampedEntity {
 
     @Column(length = 512)
     private String badgeImageUrl;
+
+    @Builder.Default
+    @Column(nullable = false)
+    private boolean isAiBadgeInProgress = false;
 
     public void increaseGivedPumati() {
         this.givedPumatiCount++;
@@ -76,6 +80,10 @@ public class Team extends TimeStampedEntity {
     public void resetPumatiCounts() {
         this.givedPumatiCount = 0L;
         this.receivedPumatiCount = 0L;
+    }
+
+    public void setAiBadgeInProgress(boolean isAiBadgeInProgress) {
+        this.isAiBadgeInProgress = isAiBadgeInProgress;
     }
 
 }
