e66440c2a310f0c5506c18c6fbee04d5884d272f
DEOKJAE KWON <juintin@kakao.com>
2025-06-17 00:45:08 +0900

    test: ApiServerApplicationTests에 ActiveProfiles("test") 적용

--- src/test/java/com/tebutebu/apiserver/ApiServerApplicationTests.java
+++ src/test/java/com/tebutebu/apiserver/ApiServerApplicationTests.java
@@ -2,8 +2,10 @@ package com.tebutebu.apiserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ApiServerApplicationTests {
 
 	@Test
